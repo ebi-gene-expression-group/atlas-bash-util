fbfad346daa6b5024d8ec4ba7b6144424ee6cab3
Robert Petryszak <rpetry@ebi.ac.uk>
2014-05-23 13:01:03 +0100

    Added applying property type/value fixes when experiment is first loaded - after retrieving idf/sdrf from zoomage

--- bash_util/generic_routines.sh
+++ bash_util/generic_routines.sh
@@ -45,17 +45,71 @@ importMageTabFromAE2() {
 
     middle=`echo $expAcc | awk -F"-" '{print $2}'`
     cp /nfs/ma/home/arrayexpress/ae2_production/data/EXPERIMENT/${middle}/${expAcc}/${expAcc}.idf.txt ${expAcc}/${expAcc}.idf.txt
-    if [ ! -e $expAcc/$expAcc.idf.txt ]; then
+    if [ ! -s $expAcc/$expAcc.idf.txt ]; then
 	echo "[ERROR] Failed to download: /nfs/ma/home/arrayexpress/ae2_production/data/EXPERIMENT/${middle}/${expAcc}/${expAcc}.idf.txt" >> $log
 	return  1
     fi
 
     middle=`echo $expAcc | awk -F"-" '{print $2}'`
     cp /nfs/ma/home/arrayexpress/ae2_production/data/EXPERIMENT/${middle}/${expAcc}/${expAcc}.sdrf.txt ${expAcc}/${expAcc}.sdrf.txt
-    if [ ! -e $expAcc/$expAcc.sdrf.txt ]; then
+    if [ ! -s $expAcc/$expAcc.sdrf.txt ]; then
 	echo "[ERROR] Failed to download: /nfs/ma/home/arrayexpress/ae2_production/data/EXPERIMENT/${middle}/${expAcc}/${expAcc}.sdrf.txt" >> $log
         return 1
     fi
 
     return 0
+}
+
+# Applies fixes encoded in $fixesFile to $exp.$fileTypeToBeFixed.txt
+applyFixes() {
+    exp=$1
+    fixesFile=$2
+    fileTypeToBeFixed=$3
+    log=$4
+
+    # Apply factor type fixes in ${fileTypeToBeFixed} file
+    for l in $(cat $ATLAS_PROD/sw/atlasprod/experiment_metadata/$fixesFile | sed 's|[[:space:]]*$||g');
+    do
+	if [ ! -s "$exp/$exp.${fileTypeToBeFixed}.txt" ]; then
+	    echo "ERROR: $exp/$exp.${fileTypeToBeFixed}.txt not found or is empty" >> $log
+	    return 1
+	fi 
+	echo $l | grep -P '\t' > /dev/null
+	if [ $? -ne 0 ]; then
+	    echo  "WARNING: line: '$l' in automatic_fixes_properties.txt is missing a tab character - not applying the fix "  >> $log
+	fi
+	correct=`echo $l | awk -F"\t" '{print $1}'`
+	toBeReplaced=`echo $l | awk -F"\t" '{print $2}'`
+	perl -pi -e "s|\t${toBeReplaced}\t|\t${correct}\t|g" $exp/$exp.${fileTypeToBeFixed}.txt
+	perl -pi -e "s|\t${toBeReplaced}$|\t${correct}|g" $exp/$exp.${fileTypeToBeFixed}.txt
+	if [ "$fixesFile" == "automatic_fixes_properties.txt" ]; then
+	    if [ "$fileTypeToBeFixed" == "sdrf" ]; then
+		#in sdrf fix factor types only
+		perl -pi -e "s|\[${toBeReplaced}\]|[${correct}]|g" $exp/$exp.${fileTypeToBeFixed}.txt
+	    fi
+	fi
+    done
+}
+
+applyAllFixesForExperiment() {
+   exp=$1
+   echo "Applying fixes for $exp ..." 
+    # Apply factor type fixes in idf file
+    applyFixes $exp automatic_fixes_properties.txt idf
+    if [ $? -ne 0 ]; then
+	echo "ERROR: Applying factor type fixes in idf file for $exp failed" 
+	return 1
+    fi
+    # Apply factor/sample characteristic type fixes to sdrf
+    applyFixes $exp automatic_fixes_properties.txt sdrf 
+    if [ $? -ne 0 ]; then
+	echo "ERROR: Applying sample characteristic/factor types fixes in sdrf file for $exp failed" 
+	return 1
+    fi
+    # Apply sample characteristic/factor value fixes in sdrf file
+    applyFixes $exp automatic_fixes_values.txt sdrf
+    if [ $? -ne 0 ]; then
+	echo "ERROR: Applying sample characteristic/factor value fixes in sdrf file for $exp failed" 
+	return 1
+    fi
 }
