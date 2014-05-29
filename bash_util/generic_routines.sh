184e9f2c64c9b6afb078be5c80921271eaa24bca
Robert Petryszak <rpetry@ebi.ac.uk>
2014-05-29 11:19:33 +0100

    Incorporated a new version of lsf_list that can  be used with an optional time out argument

--- bash_util/generic_routines.sh
+++ bash_util/generic_routines.sh
@@ -8,31 +8,20 @@ IFS="
 # and re-tries it if it doesn't come back within 10 seconds.
 lsf_process_running() {
     arg=$1
-    rm -rf lsf_list.$arg.aux
-    lsf_list > lsf_list.$arg.aux &
-    bgProcId=$!
-    while [ ! -f lsf_list.$arg.aux ]; do
-	# Sleep for 1 sec - to let lsf_list complete if it's to respond quickly
-	sleep 1
-	ps $bgProcId > /dev/null
-	if [ $? -eq 0 ]; then
-	    echo "lsf_list ($bgProcId) is still running - sleep for 10 secs..."
-	    sleep 10
-	    if [ $? -eq 0 ]; then
-	        # The background process is _still_ running - kill it and re-start
-	        kill -9 $bgProcId > /dev/null 2>&1
-		if [ $? -eq 0 ]; then 
-		    # If $bgProcId was in fact killed, start another one
-		    lsf_list > lsf_list.$arg.aux &
-		    bgProcId=$!
-		fi
-	    fi
+
+    lsfListOutput=`lsf_list 10`
+    tries=1
+    while [ $? -ne 0 ]; then
+        tries=$[$tries+1]
+	if [ $tries -le 5 ]; then 
+            lsfListOutput=`lsf_list 10`
+	else
+	    echo "ERROR: Tried to run lsf_list $tries times - it timed out ever time"  >&2
+	    return 255
 	fi
     done
-    grep $arg lsf_list.$arg.aux > /dev/null
-    ret=$?
-    rm -rf lsf_list.$arg.aux
-    return $ret
+    echo $lsfListOutput | grep "$arg" > /dev/null
+    return $?
 }
 
 function capitalize_first_letter {
