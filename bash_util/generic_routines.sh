0022b069322cf22a1a401a7de20376f1d7c18908
Robert Petryszak <rpetry@ebi.ac.uk>
2014-06-19 08:27:03 +0100

    Further attempt to prevent lsf_list from hanging

--- bash_util/generic_routines.sh
+++ bash_util/generic_routines.sh
@@ -4,36 +4,28 @@ IFS="
 "
 
 # This procedure uses lsf_list to check if process $arg is running. Because lsf_list has been known to hang (presumably because
-# of the underlying bjobs hanging when the LSF cluster is very busy), this function runs lsf_list in the background, and kills
-# and re-tries it if it doesn't come back within 5 seconds.
+# of the underlying bjobs hanging when the LSF cluster is very busy), this function includes 5 re-tries
 lsf_process_running() {
     arg=$1
-    rm -rf lsf_list.$arg.aux
-    lsf_list > lsf_list.$arg.aux &
-    bgProcId=$!
-    while [ ! -f lsf_list.$arg.aux ]; do
-	# Sleep for 1 sec - to let lsf_list complete if it's to respond quickly
-	sleep 1
-	ps $bgProcId > /dev/null
-	if [ $? -eq 0 ]; then
-	    echo "lsf_list ($bgProcId) is still running - sleep for 5 secs..."
-	    sleep 5
-	    ps $bgProcId > /dev/null
-	    if [ $? -eq 0 ]; then
-	        # The background process is _still_ running - kill it and re-start
-	        kill -9 $bgProcId > /dev/null 2>&1
-		if [ $? -eq 0 ]; then
-		    # If $bgProcId was in fact killed, start another one
-		    lsf_list > lsf_list.$arg.aux &
-		    bgProcId=$!
-		fi
-	    fi
+    tries=1
+    lsfListOutput=`lsf_list 5`
+    exitStatus=$?
+    while [ "$exitStatus" -ne 0 ]; do
+        tries=$[$tries+1]
+	if [ $tries -le 5 ]; then
+            lsfListOutput=`lsf_list 5`
+	    exitStatus=$?
+	else
+	    echo "ERROR: Tried to run lsf_list $tries times - it timed out every time"  >&2
+	    break
 	fi
     done
-    grep $arg lsf_list.$arg.aux > /dev/null
-    ret=$?
-    rm -rf lsf_list.$arg.aux
-    return $ret
+    if [ "$exitStatus" -ne 0 ]; then
+	return 255
+    else
+	echo $lsfListOutput | grep "$arg" > /dev/null
+	return $?
+    fi
 }
 
 function capitalize_first_letter {
