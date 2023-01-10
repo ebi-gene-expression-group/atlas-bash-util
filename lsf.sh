d6ebbe536b6369799ff706138a7cdeb29e4f6f8e
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2023-01-10 12:07:26 +0000

    add comment

--- lsf.sh
+++ lsf.sh
@@ -29,9 +29,12 @@ lsf_submit(){
     if [ -n "$workingDir" ]; then workingDir=" -cwd \"$workingDir\""; fi
     if [ -n "$condaEnv" ]; then
         condaBase=$(conda info --json | awk '/conda_prefix/ { gsub(/"|,/, "", $2); print $2 }')
+        #condaCmd=". ${condaBase}/bin/activate ${condaBase}/envs/${condaEnv}"
+        ###### the lines below were added to enable conda env loading in running node
         condaEnvPath="${condaBase}/envs/${condaEnv}"
         if [ -d "$condaEnv" ]; then condaEnvPath="$condaEnv"; fi
         condaCmd=". ${condaBase}/bin/activate ${condaEnvPath}"
+        ######
         commandString="${condaCmd} && ${commandString}"
     fi
     if [ -n "$logPrefix" ]; then 
