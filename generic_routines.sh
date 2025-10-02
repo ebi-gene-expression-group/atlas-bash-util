cb919274381653a5ceb297462e399e677d5bf406
Anil Thanki <thanki.anil@gmail.com>
2025-10-02 10:25:33 +0100

    Adding echo statements

--- generic_routines.sh
+++ generic_routines.sh
@@ -531,5 +531,8 @@ get_experiment_target_dir() {
 #We only condense the sdrf after successful analysis, and analysis needs organism name in e.g. GSEA, so we can't always use this
 #This assumes the organism field there is unique (it fetches the first one that appears otherwise)
 get_organism_from_condensed_sdrf(){
-    cut -f 4,5,6 $1 | grep characteristic$'\t'organism$'\t' | head -n1 | cut -f 3 | to_ensembl_species_lowercase
+    echo "DEBUG: Processing condensed SDRF file: $1" >&2
+    result=$(cut -f 4,5,6 $1 | grep characteristic$'\t'organism$'\t' | head -n1 | cut -f 3 | to_ensembl_species_lowercase)
+    echo "DEBUG: Extracted organism: $result" >&2
+    echo "$result"
 }
